102064d38b03d31831f741ae597539324a77e5ad
kaidux22 <kaidux22@gmail.com>
2023-03-10 10:47:53 +0300

    Incorrect ans

--- LegFunc.h
+++ LegFunc.h
@@ -1,42 +1,68 @@
 #include <cmath>
 
-double* LegPol(double arg, int ord) {
-	double* pols = new double[ord];
-	pols[0] = 1;
+class LegFunc {
+public:
+	LegFunc(int n, int m, double arg) {
+		mOrd = n;
+		mPwr = m;
+		mFuncs = new double* [mPwr + 1];
+		mFuncs[0] = LegPol(arg, mOrd);
+		
+		for (int idx = 1; idx <= mPwr; idx++) {
+			mFuncs[idx] = new double[mOrd + 1];
+			mFuncs[idx][0] = 0;
+		}
+
+		if (!mOrd || !mPwr) {
+			return;
+		}
+		mFuncs[1][1] = sqrt(1 - arg * arg);
+
+		for (int ord = 2; ord <= mOrd; ord++) {
+			mFuncs[1][ord] = ((double)(2 * mOrd + 3) * arg * mFuncs[1][ord - 1] - (double)(n + m + 1) * mFuncs[1][ord - 2]) / ((double)(n + 2 - m));
+		}
+
+		for (int pwr = 2; pwr <= mPwr; pwr++) {
+			for (int ord = 1; ord <= mOrd; ord++) {
+				mFuncs[pwr][ord] = ((double)(2 * m + 2) * arg * mFuncs[pwr - 1][ord]) / (sqrt(1 - arg * arg)) - (double)((n - m) * (n + m + 1)) * mFuncs[pwr - 2][ord];
+			}
+		}
+
+	}
 	
-	if (ord == 0)
+	void PrintMaxtrix() {
+		for (int pwr = 0; pwr <= mPwr; pwr++) {
+			for (int ord = 0; ord <= mOrd; ord++) {
+				cout << mFuncs[pwr][ord] << " ";
+			}
+			cout << endl;
+		}
+	}
+
+	~LegFunc() {
+		for (int idx = 0; idx <= mPwr; idx++) {
+			delete[] mFuncs[idx];
+		}
+		delete[] mFuncs;
+	}
+
+private:
+
+	double* LegPol(double arg, int ord) {
+		double* pols = new double[ord + 1];
+		pols[0] = 1;
+
+		if (ord == 0)
+			return pols;
+
+		pols[1] = arg;
+
+		for (int i = 1; i < ord; i++) {
+			pols[i + 1] = ((double)(2 * i + 1) * arg * pols[i] - (double)(i)*pols[i - 1]) / (double)(i + 1);
+		}
 		return pols;
-
-	pols[1] = arg;
-
-	for (int i = 1; i < ord - 1; i++) {
-		pols[i + 1] = ((double)(2 * i + 1) * arg * pols[i] - (double)(i)*pols[i - 1]) / (double)(i + 1);
-	}
-	return pols;
-}
-
-double **LegFunc(double arg, int ord, int pwr) {
-	double** funcs = new double*[pwr];
-
-	funcs[0] = LegPol(arg, ord);
-
-
-	for (int i = 1; i < pwr; i++) {
-		funcs[i] = new double[ord];
-		funcs[i][0] = 0;
 	}
 
-	if (ord == 0 || pwr == 0) {
-		return funcs;
-	}
-
-	funcs[1][1] = sqrt(1 - arg * arg);
-
-	for (int i = 1; i < ord - 1; i++) {
-
-	}
-	
-
-
-	return funcs;
-}
+	double** mFuncs;
+	int mOrd, mPwr;
+};
