65bbf82a67712d11dd0b98ee7fddbea4c5c15c71
lenanaidyonova <lenanaidyonova123@mail.ru>
2023-03-13 01:31:21 +0300

    yyyy

--- DormanPrince.cpp
+++ DormanPrince.cpp
@@ -10,7 +10,7 @@ float func(float x){
 }
 
 
-std::vector<long double> DormanPrince(long double x0, long double y0, long double h, long double x){
+vector<pair<long double, long double>> DormanPrince(long double x0, long double y0, long double h, long double x1){
 
     vector<vector <long double>> a = {{0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
                              {1.0 / 5.0, 0.0, 0.0, 0.0, 0.0, 0.0},
@@ -22,17 +22,17 @@ std::vector<long double> DormanPrince(long double x0, long double y0, long doubl
 
     vector <long double> b1 = {5179.0 / 57600.0, 0.0, 7571.0 / 16695.0, 393.0 / 640.0, -92097.0 / 339200.0, 187.0 / 2100.0, 1.0 / 40.0};
     vector <long double> b = {35.0 / 384.0, 0.0, 500.0 / 1113.0, 125.0 / 192.0, -2187.0 / 6784.0, 11.0 / 84.0, 0.0};
-    vector <long double> k = {0,0,0,0,0,0,0,0};
+    vector <long double> k = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
 
     long double yk = y0, zk = y0;
-    long double tol = 0.0001, err;
-    vector<long double> y; y.push_back(y0);
-    long double hmax = 0.2; long double facmax = 1.0;
-    long double fac = 0.8; long double facmin = 0.6;
-    long double gamma;              //??? надо выбрать константы
+    long double tol = 0.000001, err;
+    vector<pair<long double, long double>> xy;
+    xy.emplace_back(x0, y0);
 
-    while (x0 < x){
-        k[0] = h * func(y0);
+    long double gamma; long double koef = 0.9;       //??? надо выбрать константы
+    k[0] = h * func(y0);
+
+    while (x0 < x1){
         k[1] = h * func( y0 + a[1][0]* k[0]);
         k[2] = h * func( y0 + a[2][0] * k[0] + a[2][1] * k[1]);
         k[3] = h * func( y0 + a[3][0] * k[0] + a[3][1] * k[1] + a[3][2] * k[2]);
@@ -41,34 +41,35 @@ std::vector<long double> DormanPrince(long double x0, long double y0, long doubl
         k[6] = h * func( y0 + a[6][0] * k[0] + a[6][1] * k[1] + a[6][2] * k[2] + a[6][3] * k[3] + a[6][4] * k[4] + a[6][5] * k[5]);
 
         for (int j =0; j < 7; j++){ //подсчет ук и zк на очередном шаге
-            yk += k[j] *b[j];
-            //cout << yk;
-            zk += k[j] *b1[j];
+            yk += k[j] * b[j];
+            zk += k[j] * b1[j];
         }
 
-        err = (yk-zk)/(sqrt(zk*zk)); //пока так(
-        gamma = min(facmax, max(facmin, pow(fac*(tol/err), 1/5)));
+        err = sqrt(pow((yk-zk), 2) / xy.size()); //пока так(
+        gamma = koef * pow((tol/err), 1/5);
 
         if (err <= tol){
             x0 = x0 + h;
             y0 = yk;
-            h = min(gamma*h, hmax);
-            y.push_back(yk);
+            h = gamma * h;
+            k[0] = k[6] * gamma;
+            xy.emplace_back(x0, yk);
         }
         else{
-            h = gamma*h;
+            k[0] = k[0] * gamma;
+            h = gamma * h;
         }
 
     }
-    return y;
+    return xy;
 }
 
 
 int main(){
     long double x0 = 0, y0 = 1, h = 0.2, x = 1;
-    std::vector <long double> y = DormanPrince(x0, y0, h, x);
-    for (int i=0; i < y.size(); i++){
-        cout << y[i] << ' ';
+    vector<pair<long double, long double>> xy = DormanPrince(x0, y0, h, x);
+    for (auto & i : xy){
+        cout << i.first << ' ' << i.second << "\n";
     }
     return 0;
 }
