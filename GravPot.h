f9934875166702203c0496e2dfe2cc90befc8e77
lenanaidyonova <90788169+lenanaidyonova@users.noreply.github.com>
2023-04-02 00:34:14 +0300

    Update GravPot.h

--- GravPot.h
+++ GravPot.h
@@ -5,13 +5,13 @@
 #include "Vnm.h"
 
 #define N_CONST 2
-#define NU_CONST 398600.4415 // ÍÏ^3/Ò^2
-#define R_CONST 6378.1363 // ÍÏ
+#define NU_CONST 398600.4415 // –∫–º^3/—Å^2
+#define R_CONST 6378.1363 // –∫–º
 #define MAX_ORD 2
 
 using namespace std;
 
-void GravPot(double *vec, ComplexNum(*dx)(LegFunc&, int, int, double*), ComplexNum(*dy)(LegFunc&, int, int, double* ), ComplexNum(*dz)(LegFunc&, int, int, double*)) {
+double GravPot(double *vec, ComplexNum(*d)(LegFunc&, int, int, double*)) {
 	assert(vec[1] != 0);
 	
 	int N = N_CONST;
@@ -20,16 +20,19 @@ void GravPot(double *vec, ComplexNum(*dx)(LegFunc&, int, int, double*), ComplexN
 	double J0 = 1.0; double J2 = -0.1082635854e-2;
 	
 	LegFunc Pmn = LegFunc(N + MAX_ORD, N + MAX_ORD, sqrt(vec[0] * vec[0] + vec[1] * vec[1]) / sqrt(vec[0] * vec[0] + vec[1] * vec[1] + vec[2] * vec[2]));
-	ComplexNum res1 = ComplexNum(0, 0);
-	ComplexNum res2 = ComplexNum(0, 0);
-	ComplexNum res3 = ComplexNum(0, 0);
+	ComplexNum res = ComplexNum(0, 0);
 
-	res1 = (ComplexNum)J0 * dx(Pmn, 0, 0, vec) + (ComplexNum)pow(R, 2) * (ComplexNum)J2 * dx(Pmn, 2, 0, vec);
-	res2 = (ComplexNum)J0 * dy(Pmn, 0, 0, vec) + (ComplexNum)pow(R, 2) * (ComplexNum)J2 * dy(Pmn, 2, 0, vec);
-	res3 = (ComplexNum)J0 * dz(Pmn, 0, 0, vec) + (ComplexNum)pow(R, 2) * (ComplexNum)J2 * dz(Pmn, 2, 0, vec);
+	res = (ComplexNum)J0 * d(Pmn, 0, 0, vec) + (ComplexNum)pow(R, 2) * (ComplexNum)J2 * d(Pmn, 2, 0, vec);
+	
+	return res.Real() * (-nu);
 
-	vec[0] = res1.Real() * (-nu);
-	vec[1] = res2.Real() * (-nu);
-	vec[2] = res3.Real() * (-nu);
+}
 
+double* GradV(double* vec, double time) {
+	double* grad = new double[3];
+	grad[0] = GravPot(vec, Vdx);
+	grad[1] = GravPot(vec, Vdy);
+	grad[2] = GravPot(vec, Vdz);
+
+	return grad;
 }
