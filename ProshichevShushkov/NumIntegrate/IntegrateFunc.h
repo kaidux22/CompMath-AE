ebef531f93bcc5d45594fb5aed90a42bc5876de0
kaidux22 <kaidux22@gmail.com>
2023-05-08 20:31:53 +0300

    Space

--- ProshichevShushkov/NumIntegrate/IntegrateFunc.h
+++ ProshichevShushkov/NumIntegrate/IntegrateFunc.h
@@ -7,6 +7,6 @@
 void DormandPrince(double JD, double h, const int N, double* vec, double a[7][7], double b[7], double** k, double c[7], void (*f)(double*, double*, double));
 
 /* Решение систему ОДУ */
-double **intergrate(double JD_start, double h, const int N, double* vec);
+double **intergrate(double JD_start, double h, const int N, double *vec);
 
 #endif //COMPMATH_INTEGRATEFUNC
